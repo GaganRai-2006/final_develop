648b10bf2fdf996742ec93d14e10f8b3fedf3f5c
gagan <xyz@gamil.com>
2025-06-26 18:09:45 +0530

    added user schema

---
+++ schema/userSchema.js
@@ -0,0 +1,47 @@
+const mongose=require('mongoose')
+
+const userSchema=new mongose.Schema({
+      firstName:{
+            type:String,
+            reqiure:[true,"first name is required"],
+            minlength:[5,"atleast length is 5"],
+            trim:true,
+            lowercase:true,
+            maxlength:[20,"length less than or equal to 20"]
+
+      },
+      lastName:{
+            type:String,
+            reqiure:[true,"last name is required"],
+            minlength:[5,"atleast length is 5"],
+            trim:true,
+            lowercase:true,
+            maxlength:[20,"length less than or equal to 20"]
+
+      },
+      mobileNumber:{
+            type:String,
+            trim:true,
+            unique:[true,"number is already in use"],
+            required:[true,"phone number must be provided"],
+            minlength:[10,"phone number should be length of 10"],
+            maxlength:[10,"phone number should be length of 10"],
+      },
+      email:{
+            type:String,
+            trim:true,
+            required:[true,"email is required"],
+            unique:[true,"email is alreday in use"],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"]
+      },
+      password:{
+            type:String,
+            required:[true,"password should be provided"],
+            minlength:[6,"minlength atleast 6"]
+      }
+},{
+      timestamps:true
+})
+
+const User=mongose.model("User",userSchema);
+module.exports=User;
