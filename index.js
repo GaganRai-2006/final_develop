9cf3ee36bace29bb885edb6d0314f954b49583a7
gagan <xyz@gamil.com>
2025-06-26 16:53:41 +0530

    added ody parser

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const dbconnect = require('./config/db_confing');
 const server=require('./config/serverConfig');
+const bodyParser=require('body-parser')
 
 const express=require('express');
 
 const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
+app.post("/ping",(req,res)=>{
+      console.log(req.body);
+      return res.json({message:"pong"});
+})
 
 
 app.listen(server.PORT,async ()=>{
